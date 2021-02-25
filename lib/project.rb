24ff072c443bcb6f13fe94828d937c6b3485bafe
minirachel <rachel.a.levinson@gmail.com>
2021-02-25 16:07:44 -0500

    commit tree da6c1fecd7331ca72a6406852894e74300c54b8a

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+require_relative './backer.rb'
+
+class Project
+
+    attr_accessor :title
+    attr_reader :backers
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(backer)
+        @backers << backer
+        backer.back_project(self)
+    end
+
+end
